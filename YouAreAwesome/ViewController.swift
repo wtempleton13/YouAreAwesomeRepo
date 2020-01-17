9ee43a57dc4afe7d18aa781f9d3c7a42d0dddafb
Will Templeton <templetw@bc.edu>
2020-01-16 21:13:48 -0500

    Initial Commit

---
+++ YouAreAwesome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouAreAwesome
+//
+//  Created by William Templeton on 1/16/20.
+//  Copyright © 2020 William Templeton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
